30d43655941882b7f3b1d79be6bbfc11ea253c61
Okan Cakmak <karl_johnsonn@protonmail.com>
2020-02-07 02:22:36 -0800

    Basic get/post api

--- services/web/manage.py
+++ services/web/manage.py
@@ -18,6 +18,12 @@ def seed_db():
     db.session.add(Todo(title="test", description="naber"))
     db.session.commit()
 
+@cli.command("seed_db")
+def seed_db_multi():
+    db.session.add(Todo(title="test", description="naber"))
+    db.session.add(Todo(title="test1", description="naber"))
+    db.session.add(Todo(title="test2", description="naber"))
+    db.session.commit()
 
 if __name__ == "__main__":
     cli()
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,21 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
+ma = Marshmallow(app)
 
 
 class Todo(db.Model):
@@ -23,14 +13,47 @@ class Todo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), unique=False, nullable=False)
-    description = db.Column(db.String(128), unique=FileExistsError, nullable=False)
-    status = db.Column(db.Boolean(), default=False, nullable=False)
+    description = db.Column(db.String(128), nullable=False)
+    status = db.Column(db.Boolean, default=False, nullable=False, server_default="false")
 
-    def __init__(self, title, description):
+    def __init__(self, title, description, status):
         self.title = title
         self.description = description
+        self.status = True if status == "true" else False
+
+    def __repr__(self):
+        return '<Todo %r %r %r>' % (self.title, self.description, self.status)
+
+
+class TodoSchema(ma.ModelSchema):
+    class Meta:
+        model = Todo
+
+
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
 
 
 @app.route("/")
 def hello_world():
-    return jsonify(hello="world")
+    return render_template("index.html")
+
+
+@app.route("/todo/<todo_id>", methods=['GET'])
+def get_todo(todo_id):
+    todo = Todo.query.get(todo_id)
+    return todo_schema.jsonify(todo)
+
+
+@app.route("/todos", methods=['POST'])
+def add_todo():
+    try:
+        title = request.json['title']
+        description = request.json['description']
+        status = request.json['status']
+        new_todo = Todo(title, description, status)
+        db.session.add(new_todo)
+        db.session.commit()
+    except Exception as e:
+        print("SIÇTI:!!!!:  {}".format(e))
+    return todo_schema.jsonify(new_todo)
